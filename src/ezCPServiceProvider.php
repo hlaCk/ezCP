cd7b002d6161e4a5dd409be72d9f4aa1c6722d79
mfahadbanantec <48119910+mfahadbanantec@users.noreply.github.com>
2019-04-04 16:59:04 +0300

    Arguments

--- src/ezCPServiceProvider.php
+++ src/ezCPServiceProvider.php
@@ -255,7 +255,7 @@ class ezCPServiceProvider extends ServiceProvider
                 "{$publishablePath}/config/ezcp.php" => config_path('ezcp.php'),
             ],
             'public' => [
-                "{$publishablePath}/assets" =>  public_path('vendor/ezcp/assets'),
+                "{$publishablePath}/assets" =>  public_path('vendor/ezcp/assets'), # ezcp assets
             ],
 
         ];
